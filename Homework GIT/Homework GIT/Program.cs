115a750efde691bab36829e36de7d4c39e79ec2d
aidexx <yermakovich.nikita@gmail.com>
2021-07-11 17:57:27 +0300

    Added calculating and displaying results

--- Homework GIT/Homework GIT/Program.cs
+++ Homework GIT/Homework GIT/Program.cs
@@ -6,22 +6,31 @@ namespace Homework_GIT
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите x");
+            Console.Write("Введите x : ");
             double x = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Введите y");
+            Console.Write("Введите y : ");
             double y = Convert.ToDouble(Console.ReadLine());
 
+            Console.Write("Введите операцию : ");
             char action = Convert.ToChar(Console.Read());
 
 
             switch (action)
             {
+                case '*':
+                    PrintResult(Multiply(x,y));
+                    break;
                 default:
                     Console.WriteLine("Введите корректный символ");
                     break;
             }
 
+        }
+
+        static void PrintResult(double res)
+        {
+            Console.WriteLine($"Результат : {res}");
         }
 
         static double Multiply(double x, double y)
